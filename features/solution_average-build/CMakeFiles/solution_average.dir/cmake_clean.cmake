2d03095c7bc7e13af7f930c3d96d5fe2138ec989
Jelle Dierckens <jelle.dierckens@ugent.be>
2015-04-07 17:24:00 +0200

    feature: script voor gemiddelde van oplossingen toegevoegd

---
+++ features/solution_average-build/CMakeFiles/solution_average.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solution_average.dir/src/main.cpp.o"
+  "/home/jelle/school/beeldverwerking/project/features/solution_average/bin/solution_average.pdb"
+  "/home/jelle/school/beeldverwerking/project/features/solution_average/bin/solution_average"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solution_average.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ features/solution_average/src/main.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include "opencv2/opencv.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "fstream"
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    cout<<argv[0]<<": "<<argc-2<<" images to process"<<endl;
+    if (argc <3) {
+        cout<<"args: outputfilename  inputfiles";
+        return 255;
+    }
+    Mat outputImage;
+    Mat inputImage;
+    ifstream instream(argv[1]);
+    if(instream) {
+        char answer;
+        while (answer!='y' && answer!='n' && answer!='j') {
+            cout<<answer;
+            cout<<"file exists! overwrite? y/n\n";
+            cin>>answer;
+            if(answer=='n') {
+                cout<<"aborted\n";
+                return 0;
+            }
+        }
+    }else {
+        cout<<"error in outputfile";
+    }
+    ofstream outfilestream(argv[1]);
+    if(!outfilestream) {
+        cout<<"file could not be created!"<<endl;
+        return 254;
+    }
+    inputImage=imread(argv[2]);
+    Mat frame32f;
+    outputImage=Mat::zeros(inputImage.rows, inputImage.cols, CV_32FC3);
+    inputImage.convertTo(frame32f, CV_32FC3);
+    outputImage+=(frame32f/(argc-2));
+    for (int i=3; i<argc; i++) {
+        inputImage=imread(argv[i]);
+        inputImage.convertTo(frame32f, CV_32FC3);
+        resize(frame32f, frame32f, outputImage.size());
+        outputImage+=(frame32f/(argc-2));
+    }
+    namedWindow("result");
+    Mat channels[3];
+    normalize(outputImage, outputImage, 0,1, NORM_MINMAX);
+    split(outputImage, channels);
+    imshow("result", channels[0]);
+
+    waitKey(0);
+}
+
